bc8598f584debd97b4889cea44d5aa9e7e38b3cd
florianhamel <florianhamel>
2021-07-22 19:39:09 +0200

    usage bonus

--- srcs/main_bonus.c
+++ srcs/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fhamel <fhamel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 13:37:01 by fhamel            #+#    #+#             */
-/*   Updated: 2021/07/22 19:20:06 by fhamel           ###   ########.fr       */
+/*   Updated: 2021/07/22 19:38:36 by fhamel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@
 int	main(int ac, char **av, char **envp)
 {
 	const char	*lim;
+	char		*usage;
 
+	usage = "usage: ./pipex infile \"cmd1\" ... \"cmdn\" outfile\n\
+OR ./pipex here_doc LIMITER \"cmd1\" ... \"cmdn\" outfile\n";
 	lim = NULL;
 	if (ft_strncmp(av[1], "here_doc", 8) == 0 && ac > 5)
 	{
@@ -24,10 +27,10 @@ int	main(int ac, char **av, char **envp)
 		start_here_doc(lim, ac, av, envp);
 	}
 	else if (ft_strncmp(av[1], "here_doc", 8) == 0)
-		write(STDERR_FILENO, "Wrong number of arguments for here_doc.\n", 40);
+		write(STDERR_FILENO, usage, ft_strlen(usage));
 	else if (ac > 4)
 		start_multi_pipex(ac, av, envp);
 	else
-		write(STDERR_FILENO, "Wrong number of arguments.\n", 27);
+		write(STDERR_FILENO, usage, ft_strlen(usage));
 	return (0);
 }
